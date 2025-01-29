d18286e69a125698a18b692a6112513f0d57d5e8
laryTgs <laryssatortato99@gmail.com>
2025-01-29 18:40:59 -0300

    att

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// tab content
+const tabBtns = document.querySelectorAll("[data-tab-btn]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
+
+let lastActiveTabBtn = tabBtns[0];
+let lastActiveTabContent = tabContents[0];
+
+const filterContent = function () {
+
+  if (!(lastActiveTabBtn === this)) {
+
+    lastActiveTabBtn.classList.remove("active");
+    lastActiveTabContent.classList.remove("active");
+
+    this.classList.add("active");
+    lastActiveTabBtn = this;
+
+    const currentTabContent = document.querySelector(`[data-tab-content="${this.dataset.tabBtn}"]`);
+
+    currentTabContent.classList.add("active");
+    lastActiveTabContent = currentTabContent;
+  }
+}
+
+addEventOnElements(tabBtns, "click", filterContent);
+
+
+//Parte da seta subindo
+const scrollToTop = () => {
+    window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+    })
+}
