567542f3ef485d5261533e2af603eaa077bc296d
laryTgs <laryssatortato99@gmail.com>
2025-02-19 18:16:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-
-// tab content
-/*const tabBtns = document.querySelectorAll("[data-tab-btn]");
-const tabContents = document.querySelectorAll("[data-tab-content]");
-
-let lastActiveTabBtn = tabBtns[0];
-let lastActiveTabContent = tabContents[0];
-
-const filterContent = function () {
-
-  if (!(lastActiveTabBtn === this)) {
-
-    lastActiveTabBtn.classList.remove("active");
-    lastActiveTabContent.classList.remove("active");
-
-    this.classList.add("active");
-    lastActiveTabBtn = this;
-
-    const currentTabContent = document.querySelector(`[data-tab-content="${this.dataset.tabBtn}"]`);
-
-    currentTabContent.classList.add("active");
-    lastActiveTabContent = currentTabContent;
-  }
-}
-
-addEventOnElements(tabBtns, "click", filterContent);*/
-
-
 // Criando o elemento do cursor
 const cursor = document.createElement("div");
 cursor.classList.add("cursor");
